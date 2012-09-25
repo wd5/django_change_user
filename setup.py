f9d257461cc414620f7a63fa5c16084cfe9f7602
Mikhail Polykovskij <glader@yandex-team.ru>
2012-09-25 11:57:45 +0600

    Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django_change_user',
+    description='Become any user without password entering.',
+    version='0.0.1',
+    packages=['django_change_user'],
+)
