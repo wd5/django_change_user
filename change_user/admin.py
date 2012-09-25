f8c985e03a95ed0101bcc115c5bbaf97aed20621
Mikhail Polykovskij <glader@yandex-team.ru>
2012-09-25 11:53:58 +0600

    Initial

---
+++ change_user/admin.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+
+
+class UserAdmin(admin.ModelAdmin):
+    actions = ['become', 'delete_selected']
+
+    def become(self, request, queryset):
+        if not request.user.is_superuser:
+            messages.add_message(request, messages.INFO, 'You are not superuser.')
+        user = queryset[0]
+        request.session['_auth_user_id'] = user.id
+        return HttpResponseRedirect('/')
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
